39373e449b2d6eca62924488b69cc392ecae613e
Poonkuzhali <104356410+PoonKuzhaliE@users.noreply.github.com>
2022-09-07 10:26:10 +0530

    Add files via upload

--- StudentInfo.java
+++ StudentInfo.java
@@ -1,17 +1,21 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class StudentInfo {
-		static class StudentsDetails {
-		private int roll_no;
+	static class StudInfo {
+		private int rollno;
 		private String name;
+		private int age;
+		private String gender;
 
 		public int getRollno() {
-			return roll_no;
+			return rollno;
 		}
 
 		public void setRollno(int rollno) {
-			this.roll_no = rollno;
+			this.rollno = rollno;
 		}
 
 		public String getName() {
@@ -38,109 +42,104 @@ public class StudentInfo {
 			this.gender = gender_;
 		}
 
-		private int age;
-		private String gender;
+		public static void main(String args[]) {
 
-		// public String toString() {
-//		return " rollno: " + m_rollno + "name: " + m_name + ", age: " + m_age + ", gender: " + m_gender;
-		// }
-	
-
-	public static void main(String[] args) {
-		System.out.println("\t\t\t\t Student Details");
-		System.out.println();
-		Scanner input = new Scanner(System.in);
-		System.out.print("Number of Student : ");
-		int num = (Integer.parseInt(input.nextLine()));
-		ArrayList<StudentsDetails> stud = new ArrayList<StudentsDetails>();
-		for (int i = 0; i < num; i++) {
-			StudentsDetails std = new StudentsDetails();
-			System.out.print("Rollno : ");
-			std.setRollno(Integer.parseInt(input.nextLine()));
-			System.out.print("Name   : ");
-			std.setName(input.nextLine());
-			System.out.print("Age    : ");
-			std.setAge(Integer.parseInt(input.nextLine()));
-			System.out.print("gender : ");
-			std.setGender(input.nextLine());
-			stud.add(std);
-			System.out.println("*** Individual details entered successfully ***");
+			Scanner s = new Scanner(System.in);
+			System.out.println(" STUDENTS DETAILS:");
 			System.out.println();
-		}
-		System.out.println("+-------+-------+-------+-------+");
-		System.out.println("|Name\t|Age\t|Roll\t|Gender\t|");
-		System.out.println("+-------+-------+-------+-------+");
-		for (int i = 0; i < stud.size(); i++) {
-			System.out.println("|"+ stud.get(i).getRollno() +"\t"+"|"+stud.get(i).getName()+"\t"+"|"+stud.get(i).getAge()+"\t"+"|"+ stud.get(i).getGender()+"\t"+"|");
-			System.out.println("+-------+--------+-----+--------+");
-		}
+			System.out.println("ENTER NO OF STUDENTS:");
+			int x = (Integer.parseInt(s.nextLine()));
+		//	ArrayList<StudInfo> stud = new ArrayList<StudInfo>();
+			LinkedList<StudInfo>stud=new LinkedList<StudInfo>();
+			for (int i = 0; i < x; i++) {
+				StudInfo obj = new StudInfo();
 
-		int menuChoice = 3;
-		do {
-			System.out.println("\t\t\tEnter a Student Details");
-			System.out.println("\t\t1. Add Student\t2. remove Students\t3. Exit");
-			System.out.println("Enter a choice: ");
-			menuChoice = input.nextInt();
-			if (menuChoice == 1) {
-				StudentsDetails std = new StudentsDetails();
-				System.out.print("Rollno : ");
-				std.setRollno(input.nextInt());
-				System.out.print("Name   : ");
-				input.nextLine();
-				std.setName(input.nextLine());
-				System.out.print("Age    : ");
-				std.setAge(input.nextInt());
-				System.out.print("gender : ");
-				input.nextLine();
-				std.setGender(input.nextLine());
+				System.out.print("ENTER THE NAME      :");
+				obj.setName(s.nextLine());
+				System.out.print("ENTER THE ROLL_NO   :");
+				obj.setRollno(Integer.parseInt(s.nextLine()));
+				System.out.print("ENTER THE AGE       :");
+				obj.setAge(Integer.parseInt(s.nextLine()));
+				System.out.print("ENTER THE GENDER    :");
+				obj.setGender(s.nextLine());
+				stud.add(obj);
 
-				stud.add(std);
-				System.out.println("***Individual details entered successfully***");
+				System.out.println("---------INDIVIDUAL DETAILS ENTERED SUCCESSFULLY--------");
 				System.out.println();
-				System.out.println("+-------+-------+-------+-------+");
-				System.out.println("|Name\t|Age\t|Roll\t|Gender\t|");
-				System.out.println("+-------+-------+-------+-------+");
-
-				System.out.println();
-				System.out.println();
-				for (int i = 0; i < stud.size(); i++) {
-					System.out.println("|" + stud.get(i).getRollno() +"\t"+ "|" + stud.get(i).getName()+"\t"+"|"+stud.get(i).getAge()+"\t"+"|" +stud.get(i).getGender()+"\t"+"|");
-					System.out.println("+-------+-------+-----+---------+");
-				}
-			} 
-			else if (menuChoice == 2) {
-				System.out.println("Enter rollno number of the Student do you want edit");
-				int i =input.nextInt();
-				
-				System.out.println("+-------+-------+-------+-------+");
-				System.out.println("|Name\t|Age\t|Roll\t|Gender\t|");
-				System.out.println("+-------+-------+-------+-------+");
-				stud.remove(i);
-				for (StudentsDetails  s: stud) {
-					
-					System.out.println(" " + s.roll_no+ "         " + s.name
-							+ "           " + s.age + "      " + s.gender);
-					System.out.println("+---------+-----------+-----+----------+");
-				}
-				
-				System.out.println("\t\t1. Remove rollno\t2. remove Name\t3. Remove age\t4. Remove gender");
 			}
 
-		 } while (menuChoice < 4);
+			System.out.println("--------------------------------------------------------------");
+			System.out.println("NAME" + "\t" + "|" + "ROLL" + "\t" + "|" + "AGE" + "\t" + "|" + "GENDER" + "\t" + "|");
+			System.out.println("--------------------------------------------------------------");
+
+			for (int i = 0; i < x; i++) {
+				System.out.println(stud.get(i).getName() + "\t" + "|" + stud.get(i).getRollno() + "\t" + "|"
+						+ stud.get(i).getAge() + "\t" + "|" + stud.get(i).getGender() + "\t" + "|");
+				System.out.println("--------------------------------------------------------------");
+			}
+
+			System.out.println();
+			for (int j = 0; j < 4; j++) {
+				System.out.println();
+				System.out.println(
+						" YOU WANT TO ADD MORE STUDENT DETAILS ENTER 1,FOR DELETE ENTER 2 AND FOR CLOSE ENTER 0");
+				int y = s.nextInt();
+				s.nextLine();
+
+				if (y == 1) {
+					StudInfo obj = new StudInfo();
+
+					System.out.print("ENTER THE NAME   :");
+					obj.setName(s.nextLine());
+
+					System.out.print("ENTER THE ROLL NO:");
+					obj.setRollno(s.nextInt());
+
+					System.out.print("ENTER THE AGE    :");
+					obj.setAge(s.nextInt());
+					s.nextLine();
+
+					System.out.print("ENTER THE GENDER :");
+					obj.setGender(s.nextLine());
+
+					stud.add(obj);
+					System.out.println("--------------------------------------------------------------");
+					System.out.println(
+							"NAME" + "\t" + "|" + "ROLL" + "\t" + "|" + "AGE" + "\t" + "|" + "GENDER" + "\t" + "|");
+					System.out.println("--------------------------------------------------------------");
+
+					for (int i = 0; i < stud.size(); i++) {
+						System.out.println(stud.get(i).getName() + "\t" + "|" + stud.get(i).getRollno() + "\t" + "|"
+								+ stud.get(i).getAge() + "\t" + "|" + stud.get(i).getGender() + "\t" + "|");
+						System.out.println("--------------------------------------------------------------");
+					}
+				}
+
+				else if (y == 2) {
+					System.out.println("ENTER THE INDEX YOU WANT TO REMOVE");
+					int z = s.nextInt();
+					stud.remove(z);
+					System.out.println("--------------------------------------------------------------");
+					System.out.println(
+							"NAME" + "\t" + "|" + "ROLL" + "\t" + "|" + "AGE" + "\t" + "|" + "GENDER" + "\t" + "|");
+					System.out.println("--------------------------------------------------------------");
+
+					for (int i = 0; i < stud.size(); i++) {
+						System.out.println(stud.get(i).getName() + "\t" + "|" + stud.get(i).getRollno() + "\t" + "|"
+								+ stud.get(i).getAge() + "\t" + "|" + stud.get(i).getGender() + "\t" + "|");
+						System.out.println("--------------------------------------------------------------");
+					}
+
+				} else if (y == 3) {
+					s.close();
+					System.out.println("Scanner is inactive");
+				} else {
+					System.out.println("INVALID INPUT.KINDLY A ENTER VALID ONE");
+					System.out.println();
+				}
+			}
+			System.out.println();
+			System.out.println("YOUR DETAILS ARE ENTERED SUCCESSFULLY");
+		}
 	}
-	}		
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			}
-	
+}
